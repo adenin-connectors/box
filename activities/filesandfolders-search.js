35577ae4019bec698113c14fb1cda406dd6cc736
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-19 10:12:20 +0100

    added daterange in '*-search.js' activity.

--- activities/filesandfolders-search.js
+++ activities/filesandfolders-search.js
@@ -11,7 +11,10 @@ module.exports = async function (activity) {
     let pageSize = parseInt(pagination.pageSize);
     let offset = (parseInt(pagination.page) - 1) * pageSize;
 
-    let url = `/search?query=${activity.Request.Query.query}&offset=${offset}&limit=${pageSize}`;
+    var dateRange = cfActivity.dateRange(activity, "today");
+
+    let url = `/search?query=${activity.Request.Query.query}&offset=${offset}&limit=${pageSize}` +
+      `&updated_at_range=${dateRange.startDate.split('.')[0]}-00:00,${dateRange.endDate.split('.')[0]}-00:00`;
 
     const response = await api(url);
 
