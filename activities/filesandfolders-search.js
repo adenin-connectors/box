3e130539e494982148a64fc825670dd343b0b316
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-27 14:16:13 +0100

    fixed cfActivity missing error.

--- activities/filesandfolders-search.js
+++ activities/filesandfolders-search.js
@@ -6,7 +6,7 @@ module.exports = async function (activity) {
     let pageSize = parseInt(pagination.pageSize);
     let offset = (parseInt(pagination.page) - 1) * pageSize;
 
-    var dateRange = cfActivity.dateRange(activity, "today");
+    var dateRange = Activity.dateRange("today");
 
     let url = `/search?query=${activity.Request.Query.query}&offset=${offset}&limit=${pageSize}` +
       `&updated_at_range=${dateRange.startDate.split('.')[0]}-00:00,${dateRange.endDate.split('.')[0]}-00:00`;
