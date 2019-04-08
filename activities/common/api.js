7b10f82f4300b3159da06d7052f451469166a0cf
olliejm <olliejm@protonmail.com>
2019-04-08 12:16:07 +0100

    prepare for azure pipeline

--- activities/common/api.js
+++ activities/common/api.js
@@ -33,7 +33,7 @@ function api(path, opts) {
     return got.stream(url, opts);
   }
 
-  return got(url, opts).catch(err => {
+  return got(url, opts).catch((err) => {
     throw err;
   });
 }
@@ -47,28 +47,28 @@ const helpers = [
   'delete'
 ];
 
-api.stream = (url, opts) => apigot(url, Object.assign({}, opts, {
+api.stream = (url, opts) => got(url, Object.assign({}, opts, {
   json: false,
   stream: true
 }));
 
 for (const x of helpers) {
   const method = x.toUpperCase();
-  api[x] = (url, opts) => api(url, Object.assign({}, opts, { method }));
-  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, { method }));
+  api[x] = (url, opts) => api(url, Object.assign({}, opts, {method}));
+  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, {method}));
 }
 
 //**maps response data*/
 api.convertResponse = function (entries) {
-  let items = [];
+  const items = [];
 
   for (let i = 0; i < entries.length; i++) {
-    let raw = entries[i];
-    let item = { id: raw.id, title: raw.name, description: raw.type, link: `https://app.box.com/${raw.type}/${raw.id}`, raw: raw }
+    const raw = entries[i];
+    const item = {id: raw.id, title: raw.name, description: raw.type, link: `https://app.box.com/${raw.type}/${raw.id}`, raw: raw};
     items.push(item);
   }
 
-  return { items: items };
+  return {items: items};
 };
 
 module.exports = api;
